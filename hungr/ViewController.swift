fcece452cf0f2512edb40f8026dda65b2804429b
Isabel Yeow <Itzaghoste@gmail.com>
2025-11-19 15:20:36 -0500

    Initial Commit

---
+++ hungr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hungr
+//
+//  Created by Isabel Yeow on 11/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hungrTests/hungrTests.swift
@@ -0,0 +1,17 @@
+//
+//  hungrTests.swift
+//  hungrTests
+//
+//  Created by Isabel Yeow on 11/19/25.
+//
+
+import Testing
+@testable import hungr
+
+struct hungrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
