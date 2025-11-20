07e5f77a6cd7fc6095161f6e869f5983fad82b22
Isabel Yeow <Itzaghoste@gmail.com>
2025-11-19 20:15:34 -0500

    resized home screen

--- hungr/MainScreen/MainView.swift
+++ hungr/MainScreen/MainView.swift
@@ -11,7 +11,6 @@ class MainView: UIView {
     
     var imageViewHome: UIImageView!
     var buttonGetStarted: UIButton!
-    var labelWelcome: UILabel!
     var buttonCamera: UIButton!
     var buttonProfile: UIButton!
     var buttonFridge: UIButton!
@@ -23,33 +22,35 @@ class MainView: UIView {
         self.backgroundColor = .systemBackground
         
         setupImageView()
-        setupCameraButton()
-        setupProfileButton()
-        setupImageView()
-        setupWelcomeLabel()
         setupGetStartedButton()
         setupFridgeButton()
         setupPantryButton()
         setupFreezerButton()
+        setupCameraButton()
+        setupProfileButton()
         
         initConstraints()
     }
     
     func setupImageView() {
-           imageViewHome = UIImageView()
-           imageViewHome.contentMode = .scaleAspectFill // Fill the entire screen
-           imageViewHome.clipsToBounds = true
-           imageViewHome.image = UIImage(named: "home_image") // Your drawn image
-           imageViewHome.translatesAutoresizingMaskIntoConstraints = false
-           self.addSubview(imageViewHome)
-       }
+        imageViewHome = UIImageView()
+        imageViewHome.contentMode = .scaleAspectFit // Changed to fit - shows entire image
+        imageViewHome.clipsToBounds = true
+        imageViewHome.image = UIImage(named: "home_image")
+        imageViewHome.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(imageViewHome)
+    }
     
     func setupCameraButton() {
         buttonCamera = UIButton(type: .system)
         buttonCamera.setImage(UIImage(systemName: "camera.fill"), for: .normal)
         buttonCamera.tintColor = .systemBlue
-        buttonCamera.backgroundColor = .systemGray6
+        buttonCamera.backgroundColor = .white.withAlphaComponent(0.9)
         buttonCamera.layer.cornerRadius = 25
+        buttonCamera.layer.shadowColor = UIColor.black.cgColor
+        buttonCamera.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonCamera.layer.shadowRadius = 4
+        buttonCamera.layer.shadowOpacity = 0.2
         buttonCamera.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonCamera)
     }
@@ -58,21 +59,14 @@ class MainView: UIView {
         buttonProfile = UIButton(type: .system)
         buttonProfile.setImage(UIImage(systemName: "person.circle.fill"), for: .normal)
         buttonProfile.tintColor = .systemBlue
-        buttonProfile.backgroundColor = .systemGray6
+        buttonProfile.backgroundColor = .white.withAlphaComponent(0.9)
         buttonProfile.layer.cornerRadius = 25
+        buttonProfile.layer.shadowColor = UIColor.black.cgColor
+        buttonProfile.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonProfile.layer.shadowRadius = 4
+        buttonProfile.layer.shadowOpacity = 0.2
         buttonProfile.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonProfile)
-    }
-    
-    
-    func setupWelcomeLabel() {
-        labelWelcome = UILabel()
-        labelWelcome.text = "Welcome to hungr!"
-        labelWelcome.font = UIFont.boldSystemFont(ofSize: 28)
-        labelWelcome.textAlignment = .center
-        labelWelcome.numberOfLines = 0
-        labelWelcome.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(labelWelcome)
     }
     
     func setupGetStartedButton() {
@@ -82,6 +76,10 @@ class MainView: UIView {
         buttonGetStarted.backgroundColor = .systemBlue
         buttonGetStarted.setTitleColor(.white, for: .normal)
         buttonGetStarted.layer.cornerRadius = 12
+        buttonGetStarted.layer.shadowColor = UIColor.black.cgColor
+        buttonGetStarted.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonGetStarted.layer.shadowRadius = 4
+        buttonGetStarted.layer.shadowOpacity = 0.2
         buttonGetStarted.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonGetStarted)
     }
@@ -90,9 +88,13 @@ class MainView: UIView {
         buttonFridge = UIButton(type: .system)
         buttonFridge.setTitle("Fridge", for: .normal)
         buttonFridge.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        buttonFridge.backgroundColor = .systemBlue
+        buttonFridge.backgroundColor = .systemBlue.withAlphaComponent(0.9)
         buttonFridge.setTitleColor(.white, for: .normal)
         buttonFridge.layer.cornerRadius = 12
+        buttonFridge.layer.shadowColor = UIColor.black.cgColor
+        buttonFridge.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonFridge.layer.shadowRadius = 4
+        buttonFridge.layer.shadowOpacity = 0.2
         buttonFridge.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonFridge)
     }
@@ -101,9 +103,13 @@ class MainView: UIView {
         buttonPantry = UIButton(type: .system)
         buttonPantry.setTitle("Pantry", for: .normal)
         buttonPantry.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        buttonPantry.backgroundColor = .systemBlue
+        buttonPantry.backgroundColor = .systemBlue.withAlphaComponent(0.9)
         buttonPantry.setTitleColor(.white, for: .normal)
         buttonPantry.layer.cornerRadius = 12
+        buttonPantry.layer.shadowColor = UIColor.black.cgColor
+        buttonPantry.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonPantry.layer.shadowRadius = 4
+        buttonPantry.layer.shadowOpacity = 0.2
         buttonPantry.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonPantry)
     }
@@ -112,44 +118,36 @@ class MainView: UIView {
         buttonFreezer = UIButton(type: .system)
         buttonFreezer.setTitle("Freezer", for: .normal)
         buttonFreezer.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        buttonFreezer.backgroundColor = .systemBlue
+        buttonFreezer.backgroundColor = .systemBlue.withAlphaComponent(0.9)
         buttonFreezer.setTitleColor(.white, for: .normal)
         buttonFreezer.layer.cornerRadius = 12
+        buttonFreezer.layer.shadowColor = UIColor.black.cgColor
+        buttonFreezer.layer.shadowOffset = CGSize(width: 0, height: 2)
+        buttonFreezer.layer.shadowRadius = 4
+        buttonFreezer.layer.shadowOpacity = 0.2
         buttonFreezer.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(buttonFreezer)
     }
     
     func initConstraints() {
         NSLayoutConstraint.activate([
-            //back image
+            // Background Image - FULL SCREEN but maintains aspect ratio
             imageViewHome.topAnchor.constraint(equalTo: self.topAnchor),
-                   imageViewHome.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-                   imageViewHome.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-                   imageViewHome.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            imageViewHome.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imageViewHome.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            imageViewHome.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             
-            // Camera Button - TOP LEFT CORNER
-            buttonCamera.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 4),
-            buttonCamera.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            // Camera Button - TOP LEFT CORNER (Brought to front with higher z-index)
+            buttonCamera.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 8),
+            buttonCamera.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             buttonCamera.widthAnchor.constraint(equalToConstant: 50),
             buttonCamera.heightAnchor.constraint(equalToConstant: 50),
             
-            // Profile Button - TOP RIGHT CORNER
-            buttonProfile.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 4),
-            buttonProfile.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            // Profile Button - TOP RIGHT CORNER (Brought to front with higher z-index)
+            buttonProfile.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 8),
+            buttonProfile.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
             buttonProfile.widthAnchor.constraint(equalToConstant: 50),
             buttonProfile.heightAnchor.constraint(equalToConstant: 50),
-            
-            // Welcome Label - higher up on screen
-            labelWelcome.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 80),
-            labelWelcome.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32),
-            labelWelcome.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32),
-            
-            // Home Image - centered
-            imageViewHome.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            imageViewHome.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -40),
-            imageViewHome.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
-            imageViewHome.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
-            imageViewHome.heightAnchor.constraint(equalTo: imageViewHome.widthAnchor, multiplier: 0.8),
             
             // Fridge Button
             buttonFridge.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -140),
@@ -172,8 +170,7 @@ class MainView: UIView {
             // Get Started Button - centered at bottom (only visible when logged out)
             buttonGetStarted.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -32),
             buttonGetStarted.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32),
-            buttonGetStarted.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32),
-            buttonGetStarted.heightAnchor.constraint(equalToConstant: 56)
+            buttonGetStarted.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32),            buttonGetStarted.heightAnchor.constraint(equalToConstant: 56)
         ])
     }
     
